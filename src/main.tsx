4774679d8d2b376510c2a5533c5e828f14b17f06
Sai Ashirwad <saiashirwadr@gmail.com>
2024-12-27 23:39:27 +0530

    wip

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 const queryClient = new QueryClient({
 	defaultOptions: {
 		queries: {
-			retry: false, // Don't retry failed queries
-			refetchOnWindowFocus: false, // Don't refetch on window focus
+			retry: false,
+			refetchOnWindowFocus: false,
 		},
 	},
 })
