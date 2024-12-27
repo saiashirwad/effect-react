19f5d552be80272bccf736150a251cbe62e485db
Sai Ashirwad <saiashirwadr@gmail.com>
2024-12-27 18:16:02 +0530

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import { Rx, useRx, useRxValue } from "@effect-rx/rx-react"
-import { UseQueryOptions, useQuery } from "@tanstack/react-query"
+import {
+	UseMutationOptions,
+	UseQueryOptions,
+	useMutation,
+	useQuery,
+} from "@tanstack/react-query"
 import { Duration, Effect, Schema as S, pipe } from "effect"
 
 class Pokemon extends S.Class<Pokemon>("Pokemon")({
@@ -25,13 +30,7 @@ const pokemonNameDebounced$ = pokemonName$.pipe(
 	Rx.debounce(Duration.seconds(1)),
 )
 
-const pokemon$ = pipe(
-	pokemonNameDebounced$,
-	Rx.transform((get) => {
-		const name = get(pokemonNameDebounced$)
-		return getPokemon(name)
-	}),
-)
+const pokemon$ = Rx.map(pokemonNameDebounced$, getPokemon)
 
 const capsMon$ = Rx.map(pokemonName$, (pn) => pn.toUpperCase())
 
@@ -47,6 +46,25 @@ function useRxQuery<T, E>({ queryFn$, ...rest }: UseRxQuery<T, E>) {
 	})
 }
 
+type UseEffectMutation<T, E, V, C> = Exclude<
+	UseMutationOptions<T, E, V, C>,
+	"mutationFn"
+> & {
+	mutationFn$: (vars: V) => Effect.Effect<T, E>
+}
+
+function useEffectMutation<T, E, V, C>({
+	mutationFn$,
+	...rest
+}: UseEffectMutation<T, E, V, C>) {
+	return useMutation({
+		mutationFn: (v) => {
+			return Effect.runPromise(mutationFn$(v))
+		},
+		...rest,
+	})
+}
+
 export default function App() {
 	const [pokemonName, setPokemonName] = useRx(pokemonName$)
 	const nameDebounced = useRxValue(pokemonNameDebounced$)
@@ -56,6 +74,28 @@ export default function App() {
 	})
 
 	const pokemonCaps = useRxValue(capsMon$)
+
+	const getPokemonName = useEffectMutation({
+		mutationFn$: (name: string) =>
+			Effect.gen(function* () {
+				console.log("getting pokemon")
+				yield* Effect.sleep("1 second")
+				const pokemon = yield* getPokemon(name)
+				console.log("got pokemon")
+				return pokemon.name
+			}),
+		onSuccess: (data) => {
+			console.log(data)
+		},
+		onError: (e, v, c) => {
+			console.log(e)
+		},
+		onMutate: (a) => {
+			return {
+				hi: "there" as const,
+			}
+		},
+	})
 
 	return (
 		<div className="container mx-auto p-4 font-mono">
@@ -69,6 +109,13 @@ export default function App() {
 				value={pokemonName}
 				onChange={(e) => setPokemonName(e.target.value)}
 			/>
+			<button
+				onClick={() => {
+					getPokemonName.mutate(pokemonName)
+				}}
+			>
+				get name
+			</button>
 			<pre>{JSON.stringify(pokemon.data, null, 2)}</pre>
 			<pre>{JSON.stringify(pokemon.error, null, 2)}</pre>
 		</div>
